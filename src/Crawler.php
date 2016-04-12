7b0903934bba13beedf5e2373fed8cd6348a2bd0
Derek McDaniel <dmcdaniel12@gmail.com>
2016-04-12 09:19:47 -0400

    Now allowing multi-curl to take place. will default to 10 URLs but can be added to do more.
    Signed-off-by: Derek McDaniel <dmcdaniel12@gmail.com>

--- src/Crawler.php
+++ src/Crawler.php
@@ -4,16 +4,62 @@ class Crawler {
 
     protected $baseUrl;
 
-    public function crawl($url) {    
-        /* Start Crawl */
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-        curl_exec($ch);
-        curl_close($ch);
-        /* End Crawl */
+    public function crawl($urls) {
+
+        // array of curl handles
+        $curly = array();
+        // data to be returned
+        $result = array();
+
+        // multi handle
+        $mh = curl_multi_init();
+
+        // loop through $data and create curl handles
+        // then add them to the multi-handle
+        foreach ($urls as $id => $d) {
+
+            $curly[$id] = curl_init();
+
+            $url = (is_array($d) && !empty($d['url'])) ? $d['url'] : $d;
+            // Sets the CURL directives
+            curl_setopt($curly[$id], CURLOPT_URL, $url);
+            curl_setopt($curly[$id], CURLOPT_HEADER, 0);
+            curl_setopt($curly[$id], CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($curly[$id], CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
+
+            // post?
+            if (is_array($d)) {
+                if (!empty($d['post'])) {
+                    curl_setopt($curly[$id], CURLOPT_POST, 1);
+                    curl_setopt($curly[$id], CURLOPT_POSTFIELDS, $d['post']);
+                }
+            }
+
+            // extra options?
+            if (!empty($options)) {
+                curl_setopt_array($curly[$id], $options);
+            }
+
+            curl_multi_add_handle($mh, $curly[$id]);
+        }
+
+        // execute the handles
+        $running = null;
+        do {
+            curl_multi_exec($mh, $running);
+        } while ($running > 0);
+
+
+        // get content and remove handles
+        foreach ($curly as $id => $c) {
+            $result[$id] = curl_multi_getcontent($c);
+            curl_multi_remove_handle($mh, $c);
+        }
+
+        // all done
+        curl_multi_close($mh);
+
+        return $result;
     }
 
 }
